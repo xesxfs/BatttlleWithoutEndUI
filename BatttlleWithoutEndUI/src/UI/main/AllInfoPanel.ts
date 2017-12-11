2b1c6c4a6544b44544151cea02bc6e81d7f00171
xesxfs <835480079@qq.com>
2017-12-12 00:34:11 +0800

    allInfoPanle

--- BatttlleWithoutEndUI/src/UI/main/AllInfoPanel.ts
+++ BatttlleWithoutEndUI/src/UI/main/AllInfoPanel.ts
@@ -1,54 +1,64 @@
-class AllInfoPanel extends eui.Component implements  eui.UIComponent {
+class AllInfoPanel extends eui.Component implements eui.UIComponent {
 	public constructor() {
 		super();
 	}
-	private list:Array<eui.Label>;
-	private textGroup:eui.Group;
-	private originHeight:number;
+	private list: Array<eui.Label>;
+	private textGroup: eui.Group;
+	private textScroller: eui.Scroller;
+	private originHeight: number;
+	private limit: number = 100;
+	private htmlParse: egret.HtmlTextParser;
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
 		this.init();
 	}
 
-	private init(){
-		this.list=[];
-		this.originHeight=this.textGroup.height;
+	private init() {
+		this.htmlParse = new egret.HtmlTextParser();
+		this.list = [];
+		this.validateNow();
+		this.originHeight = this.textGroup.height;
 	}
 
-	public addText(info:string){
-	
-		let date =new Date();
-		let time ="["+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+"]";
-		let txtLab=new eui.Label(time+info);
-		txtLab.textColor=0x000000
+	public addText(info: string) {
+		if (this.list.length >= this.limit) {
+			let lab = this.list.shift();
+			this.textGroup.removeChild(lab);
+		}
+		let date = new Date();
+		let time = "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+		let txtLab = new eui.Label();
+		// this.htmlParse.parse(time+info);
+		// txtLab.size = 18;
+		txtLab.textFlow = this.htmlParse.parser(time + info);
+		txtLab.textColor = 0x000000
 		this.textGroup.addChild(txtLab);
 		this.list.push(txtLab);
 		this.tidy();
-
 	}
 
-	private tidy(){
-		let length =this.list.length;
-		let i=0;
-		while(i<length){
-			this.list[i].x=10;
-			this.list[i].y=i*this.list[i].height+20;
-			console.log("this.list[i].y:",this.list[i].y);
+	private tidy() {
+		let length = this.list.length;
+		let i = 0;
+		let lastY = 0;
+		while (i < length) {
+			this.list[i].x = 10;
+			lastY = this.list[i].y = i * this.list[i].height;
+			// console.log("this.list[i].y:", this.list[i].y);
 			i++;
 		}
-		if(length>21){
-			this.textGroup.y-=30;
-			console.log("this.textGroup.y",this.textGroup.y);
+		if (lastY >= this.originHeight && length < this.limit) {
+			this.textScroller.viewport.scrollV += 30;
+
 		}
-		console.log("list:",length);
+		console.log("this.textGroup.height:", this.textGroup.height, " lastY:", lastY);
+		console.log("list:", length);
 	}
-	
+
 }
--- BatttlleWithoutEndUI/src/UI/main/MainScene.ts
+++ BatttlleWithoutEndUI/src/UI/main/MainScene.ts
@@ -2,14 +2,15 @@ class MainScene extends BaseScene {
 	public constructor() {
 		super();
 	}
-	private allnfoPanel:AllInfoPanel;
-	private testBtn:eui.Button;
-	private c:number=0;
+	private allnfoPanel: AllInfoPanel;
+	private testBtn: eui.Button;
+	private c: number = 0;
+	private htlmStr = `<font color=0x336699 strokecolor=0x6699cc stroke=2>{%s}</font>`;
 
 	protected childrenCreated(): void {
-		this.testBtn.addEventListener("touchTap",()=>{
-			this.allnfoPanel.addText("你好旅行者！！"+this.c++);
-		},this)
+		this.testBtn.addEventListener("touchTap", () => {
+			this.allnfoPanel.addText(this.htlmStr.replace("{%s}", "你好旅行者！！" + this.c++));
+		}, this)
 
 	}
 
